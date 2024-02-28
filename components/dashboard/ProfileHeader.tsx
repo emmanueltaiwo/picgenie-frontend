21a366b71d6cec3e011934443be831cea174adab
Emmanuel Taiwo <devemmanuel1@gmail.com>
2024-02-28 16:08:33 +0100

    chore: built success modal for succesful payment

--- components/dashboard/ProfileHeader.tsx
+++ components/dashboard/ProfileHeader.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { updateUserCredits } from "@/services/profile";
 import { Profile } from "@/typings";
-import Image from "next/image";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 import { useToast } from "@/components/ui/use-toast";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import Image from "next/image";
+
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 
 const ProfileHeader = ({ user }: { user: Profile }) => {
+  const [creditPurchasModalVisible, setCreditPurchasModalVisible] =
+    useState<boolean>(false);
   const searchParams = useSearchParams();
   const status = searchParams.get("status");
   const { getItem, removeItem } = useLocalStorage("credits-bought");
@@ -40,7 +52,7 @@ const ProfileHeader = ({ user }: { user: Profile }) => {
             description: response.message,
           });
         }
-
+        setCreditPurchasModalVisible(true);
         return toast({
           title: response.message,
           description: "Refresh The Page For Changes To Begin",
@@ -55,6 +67,33 @@ const ProfileHeader = ({ user }: { user: Profile }) => {
 
   return (
     <div className="max-w-3xl">
+      {creditPurchasModalVisible && (
+        <AlertDialog defaultOpen={true}>
+          <AlertDialogContent className="flex flex-col gap-3 justify-center items-center text-center">
+            <AlertDialogHeader>
+              <Image
+                src="/confetti.png"
+                width={150}
+                height={150}
+                alt="confetti"
+                className="mx-auto"
+              />
+              <AlertDialogTitle>YAY!</AlertDialogTitle>
+              <AlertDialogDescription>
+                Your credit has been purchased succesfully
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel
+                onClick={() => setCreditPurchasModalVisible(false)}
+              >
+                Close
+              </AlertDialogCancel>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
+
       <div className="flex items-center gap-3">
         <div className="w-fit h-fit p-3 rounded-full border-2 border-[#ff80b5]">
           <Image
